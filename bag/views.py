d7adff1c6824c4084c4890433d04180e07b1f1e0
nataliiasolomchak21 <natalia2001727@gmail.com>
2023-12-02 20:28:56 +0000

    Added quantity input +/- buttons

--- bag/views.py
+++ bag/views.py
@@ -2,37 +2,46 @@ from django.shortcuts import render, redirect
 
 # Create your views here.
 
-def view_bag(request):
-    """ A view that renders the bag contents page """
 
-    return render(request, 'bag/bag.html')
+def view_bag(request):
+    """A view that renders the bag contents page"""
+
+    return render(request, "bag/bag.html")
+
 
 # Define a function that adds a quantity of a product to a shopping bag
 def add_to_bag(request, item_id):
-    """ Add a quantity of the specified product to the shopping bag """
+    """Add a quantity of the specified product to the shopping bag"""
 
     # Get the quantity of the product from the user's input
-    quantity = int(request.POST.get('quantity'))
+    quantity = int(request.POST.get("quantity"))
 
     # Get the URL to redirect to after updating the bag
-    redirect_url = request.POST.get('redirect_url')
+    redirect_url = request.POST.get("redirect_url")
     size = None
-    if 'product_size' in request.POST:
-        size = request.POST['product_size']
+    # Check if 'product_size' is present in the user's input (from a form submission)
+    if "product_size" in request.POST:
+    # If 'product_size' is present, get its value
+        size = request.POST["product_size"]
 
     # Get the current shopping bag from the user's session, or create an empty one if not present
-    bag = request.session.get('bag', {})
-
+    bag = request.session.get("bag", {})
+    # Check if a size is specified for the product
     if size:
-        if item_id in list(bag.keys()):
-            if size in bag[item_id]['items_by_size'].keys():
-                bag[item_id]['items_by_size'][size] += quantity
-            else:
-                bag[item_id]['items_by_size'][size] = quantity
-        else:
-            bag[item_id] = {'items_by_size': {size: quantity}}
-    else:
         # Check if the product is already in the bag
+        if item_id in list(bag.keys()):
+            # Check if the specified size is already in the product's sizes in the bag
+            if size in bag[item_id]["items_by_size"].keys():
+                # If yes, increase the quantity of the product with the specified size in the bag
+                bag[item_id]["items_by_size"][size] += quantity
+            else:
+                # If the size is not present for the product, add a new entry for the size with the specified quantity
+                bag[item_id]["items_by_size"][size] = quantity
+        else:
+            # If the product is not in the bag, create a new entry with the specified size and quantity
+            bag[item_id] = {"items_by_size": {size: quantity}}
+    else:
+        # If no size is specified, check if the product is already in the bag
         if item_id in list(bag.keys()):
             # If yes, increase its quantity in the bag
             bag[item_id] += quantity
@@ -40,11 +49,8 @@ def add_to_bag(request, item_id):
             # If not, add a new entry for the product with the specified quantity
             bag[item_id] = quantity
 
-    # Update the shopping bag in the user's session
-    request.session['bag'] = bag
+        # Update the shopping bag in the user's session
+        request.session["bag"] = bag
 
-    # Redirect the user to the specified URL
-    return redirect(redirect_url)
-
-
-    
+        # Redirect the user to the specified URL
+        return redirect(redirect_url)

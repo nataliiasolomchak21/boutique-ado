7859e4f328482955470d0b0389deadc504b6eef9
nataliiasolomchak21 <natalia2001727@gmail.com>
2023-12-02 15:27:14 +0000

    Added add to bag functionality

--- bag/contexts.py
+++ bag/contexts.py
@@ -1,21 +1,34 @@
+# Import the Decimal class from the decimal module to handle precise decimal arithmetic
 from decimal import Decimal
+
+# Import the 'settings' module from 'django.conf' to access project settings
 from django.conf import settings
 
+# Define a function named 'bag_contents' that takes a 'request' as a parameter
 def bag_contents(request):
 
+    # Initialize an empty list to store bag items
     bag_items = []
+    
+    # Initialize variables for total cost, total product count, and grand total
     total = 0
     product_count = 0
 
+    # Check if the total cost is less than the FREE_DELIVERY_THRESHOLD from project settings
     if total < settings.FREE_DELIVERY_THRESHOLD:
+        # Calculate the delivery cost based on a percentage of the total
         delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
+        # Calculate the remaining amount for free delivery
         free_delivery_delta = settings.FREE_DELIVERY_THRESHOLD - total
     else:
+        # If the total is equal or exceeds the FREE_DELIVERY_THRESHOLD, set delivery cost and delta to zero
         delivery = 0
         free_delivery_delta = 0
     
+    # Calculate the grand total by adding delivery cost to the total
     grand_total = delivery + total
     
+    # Create a context dictionary containing relevant information
     context = {
         'bag_items': bag_items,
         'total': total,
@@ -26,4 +39,5 @@ def bag_contents(request):
         'grand_total': grand_total,
     }
 
+    # Return the context dictionary
     return context

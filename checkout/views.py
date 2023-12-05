9de1e8db1f76510e724ff17c3c5b42e69a2e11ee
nataliiasolomchak21 <natalia2001727@gmail.com>
2023-12-05 13:21:48 +0000

    Added stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51OJyBZLDXfeZuIluVChtkPuGC18AfmecBXtdey7YLn34t73syTUDqMb3cvgbn2KGEd9yVgkh77Zk41GLOzg5a1yY007YAya5ge',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)

162926df26134901a1e00e364f9c9af49fb6e683
nataliiasolomchak21 <natalia2001727@gmail.com>
2023-12-07 11:00:36 +0000

    Added delete product functionality

--- products/views.py
+++ products/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
 from django.db.models.functions import Lower
+
 from .models import Product, Category
 from .forms import ProductForm
 
@@ -9,11 +10,6 @@ from .forms import ProductForm
 
 def all_products(request):
     """ A view to show all products, including sorting and search queries """
-    """
-    Defining a Django view function named all_products to display all products, 
-    including handling sorting and search queries. Initializing variables products, 
-    query, categories, sort, and direction with default values.
-    """
 
     products = Product.objects.all()
     query = None
@@ -21,14 +17,6 @@ def all_products(request):
     sort = None
     direction = None
 
-    """
-    Checking if there are any parameters in the GET request.
-    If 'sort' is present, extracting the sorting key from the GET parameters. 
-    If the sorting key is 'name', it's converted to 'lower_name' and products 
-    are annotated with the lowercased name. Sorting is then done based on the selected key.
-    If 'direction' is present, extracting the sorting direction. If it's 'desc', 
-    the sorting key is prefixed with '-' to indicate descending order. Sorting the products accordingly.
-    """
     if request.GET:
         if 'sort' in request.GET:
             sortkey = request.GET['sort']
@@ -43,24 +31,12 @@ def all_products(request):
                 if direction == 'desc':
                     sortkey = f'-{sortkey}'
             products = products.order_by(sortkey)
-        
-        """
-        If 'category' is present in the GET parameters, extracting 
-        and splitting the category names. Filtering the products to include 
-        only those in the specified categories and updating the categories 
-        variable with the corresponding Category objects.
-        """
+            
         if 'category' in request.GET:
             categories = request.GET['category'].split(',')
             products = products.filter(category__name__in=categories)
             categories = Category.objects.filter(name__in=categories)
 
-        """
-        If 'q' (search term) is present in the GET parameters, extracting the search query. 
-        If no query is provided, showing an error message and redirecting to the 'products' view.
-        Constructing a complex query using the Q class to search for products with names or 
-        descriptions containing the search term. Filtering the products based on the constructed query.
-        """
         if 'q' in request.GET:
             query = request.GET['q']
             if not query:
@@ -70,12 +46,6 @@ def all_products(request):
             queries = Q(name__icontains=query) | Q(description__icontains=query)
             products = products.filter(queries)
 
-    """
-    Creating a string current_sorting to represent the current sorting configuration.
-    Creating a dictionary context containing data to be passed to the template, including
-    the filtered products, search term, current categories, and sorting configuration.
-    Rendering the 'products.html' template with the provided context.
-    """
     current_sorting = f'{sort}_{direction}'
 
     context = {
@@ -87,12 +57,7 @@ def all_products(request):
 
     return render(request, 'products/products.html', context)
 
-"""
-Using get_object_or_404 to get the product with the specified product_id 
-or return a 404 response if the product is not found.
-Creating a dictionary context containing the product to be passed to the template.
-Rendering the 'product_detail.html' template with the provided context.
-"""
+
 def product_detail(request, product_id):
     """ A view to show individual product details """
 
@@ -110,9 +75,9 @@ def add_product(request):
     if request.method == 'POST':
         form = ProductForm(request.POST, request.FILES)
         if form.is_valid():
-            form.save()
+            product = form.save()
             messages.success(request, 'Successfully added product!')
-            return redirect(reverse('add_product'))
+            return redirect(reverse('product_detail', args=[product.id]))
         else:
             messages.error(request, 'Failed to add product. Please ensure the form is valid.')
     else:
@@ -148,3 +113,11 @@ def edit_product(request, product_id):
     }
 
     return render(request, template, context)
+
+
+def delete_product(request, product_id):
+    """ Delete a product from the store """
+    product = get_object_or_404(Product, pk=product_id)
+    product.delete()
+    messages.success(request, 'Product deleted!')
+    return redirect(reverse('products'))

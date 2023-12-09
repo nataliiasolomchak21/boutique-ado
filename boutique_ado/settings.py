72cdb956001aa0367898947189f5a210d50cff94
nataliiasolomchak21 <natalia2001727@gmail.com>
2023-12-09 18:42:59 +0000

    Quick fix

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -117,7 +117,7 @@ ACCOUNT_USERNAME_MIN_LENGTH = 4
 LOGIN_URL = '/accounts/login/'
 LOGIN_REDIRECT_URL = '/'
 
-WSGI_APPLICATION = 'boutique_ado.wsgi.application'
+WSGI_APPLICATION = 'boutique_ado_online_shop.wsgi:application'
 
 
 # Database
